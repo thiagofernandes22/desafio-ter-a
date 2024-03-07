8f3c7734e8dff65c85ea9172347107971e27288f
thiagofernandes22 <thiagooliverfvo@gmail.com>
2024-03-07 19:14:55 -0300

    first commit

---
+++ src/components/zod.js
@@ -0,0 +1,9 @@
+import {z} from 'zod'
+
+export const cepFormSchema = z.object({
+    cep: z.string()
+    .min(8, 'CEP deve ter 8 dígitos')
+    .refine((value) => /^\d{5}\d{3}$/.test(value), {
+        message: "CEP inválido, deve ter o formato 123345678"
+    })
+})
